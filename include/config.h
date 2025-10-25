6e373dbda35a5a83bfdbd78e57380894ab8cba69
sheigl99 <sheila.gomezlopez@studmail.htw-aalen.de>
2025-10-25 16:16:29 +0200

    Update config.h

--- include/config.h
+++ include/config.h
@@ -2,11 +2,11 @@
 #define CONFIG_H
 
 // WLAN-Zugangsdaten
-const char *SSID = "Vodafone-39B6";
-const char *PASSWORD = "LJcBBm7ADpFNaF6C";
+const char *SSID = "*********";
+const char *PASSWORD = "*****************";
 
 // Telegram Bot-Daten
-const char *BOT_TOKEN = "7954908829:AAHE4UeADxaweUJWv_1TW5Ea7xXdfWd_08E";
-const char *CHAT_ID = "5715853990";
+const char *BOT_TOKEN = "*******************************";
+const char *CHAT_ID = "*********";
 
 #endif
